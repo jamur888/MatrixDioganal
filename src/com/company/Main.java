550646a4d951e97b32fc66152679a8b27194cb1d
Jamshit <muradow.jamshit@bk.ru>
2021-10-21 19:10:26 +0300

    add Class Arrays

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -7,7 +7,7 @@ public class Main {
     public static void main(String[] args) {
         // write your code her
 //диоганал с лева  самого нижего угла
-        Arrays run = new Arrays();
+        Arrays run = new Arrays ();
         run.run();
     }
 }
